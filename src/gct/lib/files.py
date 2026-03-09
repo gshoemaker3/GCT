108a85601616332ad43d5a9ae0b199fa1ffcf65c
gshoemaker <garrett.shoemaker3@gmail.com>
2026-03-08 22:04:02 -0600

    my demo

--- src/gct/lib/files.py
+++ src/gct/lib/files.py
@@ -18,7 +18,9 @@ def copy_file(is_interactive: bool, src: str = None, dst: str = None) -> None:
         - dst: this is the destination for the copied file if provided from the command line.
     """
 
-    BadVariableSyntax = 53
+    print(
+        "4444.                                444444444777777777767676676767676676888888888888888888888888"
+    )
 
     if is_interactive:
         print("Please provide the source path for the file to be copied")
